762eac6b481da560eebca40ed9294f0e57397876
Nil Ventosa <ventosa@gmail.com>
2023-01-01 13:56:27 +0100

    Add netrw.nvim back

--- lua/nil/remap.lua
+++ lua/nil/remap.lua
@@ -7,6 +7,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", "10jzz")
+vim.keymap.set("n", "<C-k>", "10kzz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -21,7 +23,6 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
@@ -29,14 +30,6 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Open files under cursor in splits
 vim.keymap.set("n", "gv", ":vertical wincmd f<CR>")
@@ -54,3 +47,5 @@ vim.keymap.set("n", "<Down>", "<C-w>-")
 vim.keymap.set("n", "<Left>", "<C-w><")
 vim.keymap.set("n", "<Right>", "<C-w>>")
 
+-- NETRW
+vim.keymap.set("n", "<C-n>", ":call ToggleNetrw()<CR>")
