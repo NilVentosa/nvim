96abca439d5056341663cff3fd28f595027e7b0d
nilventosa <ventosa@gmail.com>
2024-12-23 16:22:59 +0100

    things

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -31,50 +31,19 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
-			lspconfig.rubocop.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
-
-			vim.keymap.set("n", "gd", function()
-				vim.lsp.buf.definition()
-			end, {})
-			vim.keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, {})
-			vim.keymap.set("n", "<leader>d", function()
-				vim.diagnostic.open_float()
-			end, {})
-			vim.keymap.set("n", "]d", function()
-				vim.diagnostic.goto_next()
-			end, {})
-			vim.keymap.set("n", "[d", function()
-				vim.diagnostic.goto_prev()
-			end, {})
-			vim.keymap.set("n", "<leader>ca", function()
-				vim.lsp.buf.code_action()
-			end, {})
-			vim.keymap.set("n", "<leader>rr", function()
-				vim.lsp.buf.references()
-			end, {})
-			vim.keymap.set("n", "<leader>rn", function()
-				vim.lsp.buf.rename()
-			end, {})
-			vim.keymap.set("i", "<C-h>", function()
-				vim.lsp.buf.signature_help()
-			end, {})
-			vim.keymap.set({ "i", "s" }, "<C-k>", function()
-				return vim.snippet.active({ direction = 1 }) and vim.snippet.jump(1)
-			end, { silent = true })
-
-			vim.keymap.set({ "i", "s" }, "<C-j>", function()
-				return vim.snippet.active({ direction = -1 }) and vim.snippet.jump(-1)
-			end, { silent = true })
+            lspconfig.ruby_lsp.setup({
+                capabilities = capabilities,
+                cmd = { "/Users/nil/.local/share/nvim/mason/bin/ruby-lsp" }
+            })
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {})
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {})
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+			vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
 		end,
 	},
 }
